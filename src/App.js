288314e38cb4eec04db5d9d10184831db68a26c6
ryokid <ryo.kawana.1@gmail.com>
2019-11-03 04:56:40 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const greeting = "Hi, Tom!";
-    const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
-    return <input type="text" onChange={() => {console.log("I am clicked.")}} />;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
   }
 }
 
