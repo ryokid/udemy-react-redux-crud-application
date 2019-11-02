e54ec14074f307d1bea46ecded38c94776adf84f
ryokid <ryo.kawana.1@gmail.com>
2019-11-03 04:29:53 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div><h1>Hello, world</h1></div>;
+class App extends Component {
+  render() {
+    return <h1>Hello, world!!</h1>;
+  }
 }
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div", 
+//       null, 
+//       "Hello, world!!"
+//     );
+//   }
+// }
 
 export default App;
